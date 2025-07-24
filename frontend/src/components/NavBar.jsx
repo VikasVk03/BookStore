293241bc91dec4bef1ff0b7f7559ad7c79474201
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-07-24 21:43:00 +0530

    navBarComplete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 // react icons 
-import { FaBlog } from "react-icons/fa6";
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6";
+
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const [isSticky, setIsSticky] = useState(false)
@@ -30,18 +31,56 @@ const NavBar = () => {
 
     // navItems here
     const navItems = [
-        {link: "Home", path: "/"},
-        {link: "About", path: "/about"},
-        {link: "Shop", path: "/shop"},
-        {link: "Sell Your Book", path: "/admin/dashboard"},
-        {link: "Blog", path: "/blog"},
+        {label: "Home", path: "/"},
+        {label: "About", path: "/about"},
+        {label: "Shop", path: "/shop"},
+        {label: "Sell Your Book", path: "/admin/dashboard"},
+        {label: "Blog", path: "/blog"},
     ]
   return (
-    <header>
-        <nav>
-            <div>
+    <header className='w-full bg-transparent fixed top-0 right-0 left-0 transition-all ease-in duration-300'>
+        <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300": ""}`}>
+            <div className='flex justify-between items-center text-base gap-8'>
                 { /* logo */}
-                <Link to='/'><FaBlog className='inline-block'/>Books</Link>
+                <Link to='/' className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block'/>Books</Link>
+
+                { /*  nav item for large device*/}
+                <ul className='md:flex space-x-12 hidden'>
+                   {navItems.map(({ label, path }) => (
+                    <li key={path}>
+                        <Link to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>
+                            {label}
+                        </Link>
+                    </li>
+                    ))}
+                </ul>
+
+                {/* btn for lg devices */}
+                <div className='space-x-12 hidden lg:flex items-center'>
+                    <button> <FaBarsStaggered className='w-5 hover:text-blue-700'/> </button>
+                </div>
+
+                {/* menu btn for the mobile devices */}
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='text-black focus:outline-none'>
+                        {
+                            isMenuOpen? <FaXmark className='h-5 w-5 text-black' /> : <FaBarsStaggered className='h-5 w-5 text-black'/>
+                        }
+                    </button>
+                </div>
+            </div>
+
+            {/* navItems for small devices */}
+            <div className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden" } `}>
+                <ul>
+                    {navItems.map(({ label, path }) => (
+                        <li key={path}>
+                            <Link to={path} className='block text-base text-white uppercase cursor-pointer hover:text-blue-700'>
+                                {label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     </header>
